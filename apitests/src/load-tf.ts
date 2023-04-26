934372091202dca6c4d748d16e9b61e52e181f1e
Sofia Berlin <sofiamberlin@gmail.com>
2023-04-26 01:21:26 -0700

    load-tf : - use env variable TF_MODULE - const MODULES holds possible values for TF_MODULE - const DEFAULT_MODULE - check for architecture type and os type - add error handling

--- apitests/src/load-tf.ts
+++ apitests/src/load-tf.ts
@@ -1,23 +1,58 @@
-/* To Import: "import * from load-tf" */
+import Os from "os";
+import * as dotenv from "dotenv";
+dotenv.config();
+/* 
+Loads a tensorflow module based on environment variable TF_MODULE
+EXPORTS: load() function, TFModule type
+( To Import this module elsewhere: "import * from load-tf" ) 
+*/
 
 /* -- Types -- */
 // private
 type GPUModule = typeof import("@tensorflow/tfjs-node-gpu");
 type JSModule = typeof import("@tensorflow/tfjs");
+type NodeModule = typeof import("@tensorflow/tfjs-node");
 // public
-export type TFModule = GPUModule | JSModule;
+export type TFModule = GPUModule | JSModule | NodeModule;
+
+/* -- Constants -- */
+
+// posible values of TF_MODULE. TF_MODULE must match a tensorflowjs module name
+const MODULES = {
+  tfjs: "tfjs",
+  "tfjs-node-gpu": "tfjs-node-gpu",
+  "tfjs-node": "tfjs-node",
+};
+const DEFAULT_MODULE = MODULES.tfjs;
 
 /* -- Functions: -- */
 
-// load(): Loads tensorflow library dynamically based on OS
+/* load(): Loads tensorflow library dynamically based on OS */
 export async function load(): Promise<TFModule> {
   try {
-    if (process.env.OS === "linux") {
-      return await import("@tensorflow/tfjs-node-gpu");
+    let module = process.env.TF_MODULE;
+    if (module === MODULES["tfjs-node-gpu"]) {
+      if (!isLinux()) throw new Error("GPU not supported on this OS");
+    } else if (module === MODULES["tfjs-node"]) {
+      if (!isArm64()) throw new Error("Node not supported on this CPU");
     } else {
-      return await import("@tensorflow/tfjs");
+      module = DEFAULT_MODULE;
     }
+    //return: Promise<GPUModule | JSModule | NodeModule>
+    return await import(`@tensorflow/${module}`);
   } catch (err) {
+    // Error handling
+    let message = "Unknown Error";
+    if (err instanceof Error) message = err.message;
+    console.error(message);
     throw new Error("Failed to load TensorFlow library");
   }
+}
+
+function isLinux(): boolean {
+  return Os.platform() === "linux";
+}
+
+function isArm64(): boolean {
+  return process.arch === "arm64";
 }

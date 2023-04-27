1cc0b82f8cd412f000b17b3a6af2dcd62b216d5d
Sofia Berlin <sofiamberlin@gmail.com>
2023-04-26 17:46:32 -0700

    Tensor.fill() : Complete

---
+++ apitests/src/creation/fill.spec.ts
@@ -0,0 +1,31 @@
+import { expect } from "chai";
+import * as loader from "../load-tf";
+import type tfTypes from "@tensorflow/tfjs-core";
+
+describe("tf.fill(shape, value, dtype?): ", async () => {
+  const tf: loader.TFModule = await loader.load();
+  it("  -- basic", () => {
+    const t: tfTypes.Tensor3D = tf.fill([3, 2, 3], 1);
+    const expected = [
+      [
+        [1, 1, 1],
+        [1, 1, 1],
+      ],
+      [
+        [1, 1, 1],
+        [1, 1, 1],
+      ],
+      [
+        [1, 1, 1],
+        [1, 1, 1],
+      ],
+    ];
+    expect(t.arraySync()).to.eql(expected);
+  });
+  it("  -- dtype", () => {
+    const t: tfTypes.Tensor2D = tf.fill([2, 1], 4, "int32");
+    const expected = [[4], [4]];
+    expect(t.dtype).to.eql("int32");
+    expect(t.arraySync()).to.eql(expected);
+  });
+});

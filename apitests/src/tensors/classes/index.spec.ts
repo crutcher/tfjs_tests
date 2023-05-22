d715ff63408375b6791267837fe502b3bc1a37b7
Sofia Berlin <sofiamberlin@gmail.com>
2023-05-22 12:28:27 -0700

    Complete class tf.Variable

--- apitests/src/tensors/classes/index.spec.ts
+++ apitests/src/tensors/classes/index.spec.ts
@@ -4,11 +4,16 @@ chai.use(tensorChaiPlugin);
 import spies from "chai-spies";
 chai.use(spies);
 import * as Tensor from "./Tensor";
+import * as Variable from "./Variable";
 
 /* ---- Creating Tensors: tf classes related to tensors ---- */
 describe("**** TENSORS: Classes ****", () => {
   /* ---- tf.Tensor class---- *
   A tf.Tensor object represents an immutable, multidimensional array of numbers that has a shape and a data type.
   */
-  describe("tf.Tensor(start, stop, num):", Tensor.run.bind(this));
+  describe("tf.Tensor : class", Tensor.run);
+  /* ---- tf.Variable class---- *
+  A mutable tf.Tensor, useful for persisting state, e.g. for training.
+  */
+  describe("tf.Variable : class", Variable.run);
 });
